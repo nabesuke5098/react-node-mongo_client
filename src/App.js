b38e1492cc923d6dbedb76e5f6cd0cba3f8d7b62
nabesuke <nabesuke5098@gmail.com>
2021-03-17 09:02:42 +0900

    add modules react-router-dom etc

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+
+import Header from './components/header/header';
+import TopPage from './pages/toppage/toppage';
+import Footer from './components/footer/footer';
 import './App.css';
 
-function App() {
-  return (
-    <div>
-      <h1>テスト環境01/03/07</h1>
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <>
+        <Header />
+        <Switch>
+          <Route exact path="/" component={TopPage} />
+        </Switch>
+        <Footer />
+      </>
+    );
+  }
 }
 
 export default App;
