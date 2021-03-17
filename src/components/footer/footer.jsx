33ce0328894d21245bb03e188473e28e17a14fc8
nabesuke <nabesuke5098@gmail.com>
2021-03-17 09:01:30 +0900

    add footer

---
+++ src/components/footer/footer.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class Footer extends React.Component {
+  render() {
+    return (
+      <div className="footer">
+        <div className="footer-inner">
+          <div className="footer-copyrights">
+            @copyrihts
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Footer;
