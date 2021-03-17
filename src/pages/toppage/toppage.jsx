8c07ce90edb53b2bed7a0423ffcee11649a507c1
nabesuke <nabesuke5098@gmail.com>
2021-03-17 09:00:59 +0900

    add toppage

---
+++ src/pages/toppage/toppage.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import { TopPageContainer } from './toppage.styles';
+
+class TopPage extends React.Component {
+  render() {
+    return (
+      <TopPageContainer>
+        <div>
+          <h1>test</h1>
+        </div>
+      </TopPageContainer>
+    );
+  }
+}
+
+export default TopPage;
